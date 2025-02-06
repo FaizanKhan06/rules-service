166c99c8537643fb51a8f56cc5aab0c7dd3ffdf1
Faizan Khan <faizankhan062002@gmail.com>
2025-02-06 09:19:40 +0530

    commit

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,9 @@
+INSERT INTO rules (
+    terms_and_conditions, 
+    contribution_per_month, 
+    term_period, 
+    interest_rate, 
+    contribution_deadline, 
+    community_start_date
+) 
+VALUES ('Standard community rules, monthly contributions required.', 100.00, 12, 5.0, '2025-02-01 00:00:00', '2025-02-05 00:00:00');
